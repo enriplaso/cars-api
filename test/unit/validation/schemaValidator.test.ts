8e45fb2c464a664032c52a8c5d29c3dc759a1914
git config --global user.email <enriplaso@gmail.com>
2021-09-11 20:38:45 +0200

    validation tests

--- test/unit/validation/schemaValidator.test.ts
+++ test/unit/validation/schemaValidator.test.ts
@@ -1,36 +1,150 @@
-/*import { expect } from 'chai';
+import { expect } from 'chai';
 import { randomUUID } from 'crypto';
 import 'mocha';
 import { Color } from '../../../src/storage/domain/ICarDomain';
-import { CarModel } from '../../../src/storage/models/CarModel';
+//import { CarModel } from '../../../src/storage/models/CarModel';
 import { SchemaValidator } from '../../../src/validation/schemaValidator';
+import { carJsonSchema, serialUUIDJsonSchema, singlePropertiesJsonSchema } from '../../../src/validation/schemas/requestSchemas';
+import Ajv from 'ajv';
+import { Request } from 'express';
+import { fail } from 'assert';
+import { CarError } from '../../../src/error/CarError';
+import { ErrorCodes } from '../../../src/error/ErrorCodes';
 
 describe('Schema validation tests', function () {
     this.timeout(120000);
     const ajv = new Ajv();
 
-    const schemaValidator = new SchemaValidator(ajv);
-    beforeEach(async function () {
-      
+    const schemaValidator = new SchemaValidator(
+        ajv.compile(carJsonSchema),
+        ajv.compile(serialUUIDJsonSchema),
+        ajv.compile(singlePropertiesJsonSchema),
+    );
+
+    it('Should not throw when a car object is valid', async function () {
+        const car = {
+            brand: 'ford',
+            color: Color.BLUE,
+            model: 'S333',
+        };
+        const req = {
+            body: car,
+        };
+
+        schemaValidator.validateCar(req as Request);
     });
 
-    afterEach(async function () {
-      
-    });
-
-    it('Should not allow additional properies', async function () {
+    it('Should allow cars with SerialUUID prop ', async function () {
         const car = {
             serialUUID: randomUUID(),
             brand: 'ford',
             color: Color.BLUE,
             model: 'S333',
+        };
+        const req = {
+            body: car,
+        };
+
+        schemaValidator.validateCar(req as Request);
+    });
+
+    it('Should not allow additional properies', async function () {
+        const car = {
+            brand: 'ford',
+            color: Color.BLUE,
+            model: 'S333',
             additionalProp: 'anyProp',
         };
-        const carModel = new CarModel(car);
-        const createdCar = await carModel.save();
+        const req = {
+            body: car,
+        };
+        try {
+            schemaValidator.validateCar(req as Request);
+            fail('should fail');
+        } catch (error) {
+            expect(error instanceof CarError).to.be.true;
+            expect((error as CarError).code).to.be.equal(ErrorCodes.Validation.Schema);
+        }
+    });
 
-        expect(createdCar.get('brand')).to.equal(car.brand);
-        expect(createdCar.get('additionalProp')).to.be.undefined;
+    it('Should not allow a color that is not in the Enum definition', async function () {
+        const car = {
+            brand: 'ford',
+            color: 'Purple',
+            model: 'S333',
+        };
+        const req = {
+            body: car,
+        };
+        try {
+            schemaValidator.validateCar(req as Request);
+            fail('should fail');
+        } catch (error) {
+            expect(error instanceof CarError).to.be.true;
+            expect((error as CarError).code).to.be.equal(ErrorCodes.Validation.Schema);
+        }
+    });
+
+    it('Should throw if required property is not included', async function () {
+        const car = {
+            brand: 'ford',
+            color: 'Purple',
+        };
+        const req = {
+            body: car,
+        };
+        try {
+            schemaValidator.validateCar(req as Request);
+            fail('should fail');
+        } catch (error) {
+            expect(error instanceof CarError).to.be.true;
+            expect((error as CarError).code).to.be.equal(ErrorCodes.Validation.Schema);
+        }
+    });
+
+    it('Should NOT throw if serialUUID is valid', async function () {
+        const req = {
+            params: { serialUUID: randomUUID() },
+        };
+        schemaValidator.validateSerialUUID(req as any);
+    });
+
+    it('Should throw if required property is not included', async function () {
+        const req = {
+            params: { serialUUID: 'segwehgzwepoj' },
+        };
+        try {
+            schemaValidator.validateSerialUUID(req as any);
+            fail('should fail');
+        } catch (error) {
+            expect(error instanceof CarError).to.be.true;
+            expect((error as CarError).code).to.be.equal(ErrorCodes.Validation.SerialUUID);
+        }
+    });
+
+    it('Should validate a single propetry', async function () {
+        const car = {
+            brand: 'ford',
+        };
+        const req = {
+            body: car,
+        };
+        schemaValidator.validateUpdateProperties(req as Request);
+    });
+
+    it('Should throw if a single property is wrong', async function () {
+        const car = {
+            notAllowedProp: 'ford',
+        };
+        const req = {
+            body: car,
+        };
+        try {
+            schemaValidator.validateUpdateProperties(req as any);
+            fail('should fail');
+        } catch (error) {
+            expect(error instanceof CarError).to.be.true;
+            expect((error as CarError).code).to.be.equal(ErrorCodes.Validation.Schema);
+        }
     });
 });
-*/
