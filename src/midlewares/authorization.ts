2bc5c9d24f95cdccb5fe3905873c37cfc58554f1
git config --global user.email <enriplaso@gmail.com>
2021-09-12 01:50:53 +0200

    authorization working

--- src/midlewares/authorization.ts
+++ src/midlewares/authorization.ts
@@ -1,1 +1,19 @@
-//import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
+
+const TOKEN_SECRET = process.env['TOKEN_SECRET'] || 'token-secret';
+
+
+export const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization
+    if (!token) res.status(StatusCodes.UNAUTHORIZED).json({error: "please provide valid token"})
+    else {
+        jwt.verify(token, TOKEN_SECRET, (err, value) => {
+            console.info(value);
+            if (err) res.status(500).json({error: 'failed to authenticate token'})
+            //req.user = value.data
+            next()
+        })
+    }
+}
--- src/routes/configRoutes.ts
+++ src/routes/configRoutes.ts
@@ -1,29 +1,37 @@
 import { Application } from 'express';
 import { CarController } from '../controller/carController';
 import { UserController } from '../controller/userController';
+import { validateToken } from '../midlewares/authorization';
 import { schemaValidation } from '../midlewares/schemaValidation';
 import { SchemaValidator } from '../validation/schemaValidator';
 
 export const configureCarRoutes = (app: Application, carController: CarController, validateCar: SchemaValidator) => {
-    app.route(`/car`).post(schemaValidation(validateCar.validateCar.bind(validateCar)), carController.createNewCar.bind(carController));
+    app.route(`/car`).post(
+        validateToken,
+        schemaValidation(validateCar.validateCar.bind(validateCar)),
+        carController.createNewCar.bind(carController),
+    );
 
     app.route(`/car/:serialUUID`).delete(
+        validateToken,
         schemaValidation(validateCar.validateSerialUUID.bind(validateCar)),
         carController.deletCarBySerialUUID.bind(carController),
     );
 
     app.route(`/car/:serialUUID`).get(
+        validateToken,
         schemaValidation(validateCar.validateSerialUUID.bind(validateCar)),
         carController.getCarBySerialUUID.bind(carController),
     );
 
     app.route(`/car/:serialUUID`).put(
+        validateToken,
         schemaValidation(validateCar.validateSerialUUID.bind(validateCar)),
         schemaValidation(validateCar.validateUpdateProperties.bind(validateCar)),
         carController.updateSingleProperties.bind(carController),
     );
 
-    app.route(`/metadata`).get(carController.getMetadata.bind(carController));
+    app.route(`/metadata`).get(validateToken, carController.getMetadata.bind(carController));
 };
 
 export const configureUserRoutes = (app: Application, userController: UserController) => {
