d469ac6aac1005e5ee0b4029fd7fce1f20cb0e2b
git config --global user.email <enriplaso@gmail.com>
2021-09-12 10:37:38 +0200

    auth middleware refactor

--- src/error/ErrorCodes.ts
+++ src/error/ErrorCodes.ts
@@ -9,19 +9,20 @@ export const ErrorCodes = {
         SerialUUID: 'com.car.validation.serialUUID.not.valid',
         General: 'com.car.validation.general.no.valid',
     },
-
     UserValidation: {
         Schema: 'com.user.validation.schema.not.valid',
         Email: 'com.user.validation.email.not.valid',
     },
-
     UserStorage: {
         General: 'com.user.storage.general',
         NoFound: 'com.user.storage.NoFound',
         AlreadyExists: 'com.user.storage.NoFound',
     },
-
     UserLogin: {
         InvalidPassword: 'com.user.login.password.invalid',
+    },
+    Authentication: {
+        NoToken: 'com.auth.token.not.provided',
+        TokenInvalid: 'com.auth.token.invalid',
     },
 };
